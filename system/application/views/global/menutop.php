e10f5a5d18b3eda9d680aee7e9c0801e27720214
mat sadler <github@redstudio.co.uk>
2010-08-20 09:48:24 +0100

    delete test

--- system/application/views/global/menutop.php
+++ system/application/views/global/menutop.php
@@ -2,17 +2,14 @@
 <div id="ja-header" class="wrap"> 
   <div class="main clearfix"> 
   
-  	  	<div class="logo-text" style="float:left"> 
+  	<div class="logo-text" style="float:left"> 
   		<h1>
   	
-<a href="<?=base_url()?>" title="channel resource"><img src="<?=base_url()?>images/logo.png"></img></a>
+<a href="<?=base_url()?>" title="channel resource"><img src="<?=base_url()?>images/proctor/logo.png"></img></a>
 
-  		
-  		 
-  				
   		</h1> 
   	</div> 
   	
-  	      </div> 
+  </div> 
 </div> 
 <!-- //HEADER --> 
