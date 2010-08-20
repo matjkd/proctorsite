cd51b91a4783e0b085b776ff9c6aa585baca68be
mat sadler <github@redstudio.co.uk>
2010-08-20 10:08:16 +0100

    some trimming here and there

--- system/application/views/global/header.php
+++ system/application/views/global/header.php
@@ -3,11 +3,13 @@
 
 <link rel="SHORTCUT ICON" href="http://www.channel-resource.com/favicon.ico">
  <link rel="shortcut icon" href="http://www.channel-resource.com/favicon.png" >
+ 
 <link rel='stylesheet' href='<?=base_url()?>css/system.css' type='text/css' />
 <link rel='stylesheet' href='<?=base_url()?>css/general.css' type='text/css' />
 <link rel='stylesheet' href='<?=base_url()?>css/typo.css' type='text/css' />
 <link rel='stylesheet' href='<?=base_url()?>css/autocomplete.css' type='text/css' />
 <link rel='stylesheet' href='<?=base_url()?>css/jquery-ui-1.8.custom.css' type='text/css' />
+<link rel='stylesheet' href='<?=base_url()?>css/demo_table_jui.css' type='text/css' />
 
 
 <script type='text/javascript' src='http://www.google.com/jsapi'></script>
@@ -15,19 +17,11 @@
 <script src="<?=base_url()?>js/Jquery.js" type="text/javascript"></script>
 <script src="<?=base_url()?>js/jeditable.js" type="text/javascript"></script>
 <script src="<?=base_url()?>js/jquery-ui-1.8.2.custom.min.js" type="text/javascript"></script>
-
-
 <script src="<?=base_url()?>js/jquery.dataTables.min.js" type="text/javascript"></script>
-
-<link rel='stylesheet' href='<?=base_url()?>css/demo_table_jui.css' type='text/css' />
-
-
-
 <script src="<?=base_url()?>js/autocomplete.js" type="text/javascript"></script>
-
 <script src="<?=base_url()?>js/AC_RunActiveContent.js" language="javascript"></script>
-
 <script src="<?=base_url()?>js/jquery.cycle.all.min.js" language="javascript"></script>
+
 
 <script language="javascript">AC_FL_RunContent = 0;</script>
 
@@ -51,7 +45,7 @@
 
 <link rel='stylesheet' href='<?=base_url()?>css/colors/default.css' type='text/css' />
 <title>
-
+Proctor Consulting
 </title>
 </head>
 
--- system/application/views/template.php
+++ system/application/views/template.php
@@ -1,16 +1,30 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-gb" lang="en-gb">
 
-<?php $this->load->view('global/header'); 
-
-
-?>
+<?php $this->load->view('global/header'); ?>
 
 <body id="bd" class="  fs3"> 
+ <!--[if lt IE 7]>
+  <div style='border: 1px solid #F7941D; background: #FEEFDA; text-align: center; clear: both; height: 75px; position: relative;'>
+    <div style='position: absolute; right: 3px; top: 3px; font-family: courier new; font-weight: bold;'><a href='#' onclick='javascript:this.parentNode.parentNode.style.display="none"; return false;'><img src='http://www.ie6nomore.com/files/theme/ie6nomore-cornerx.jpg' style='border: none;' alt='Close this notice'/></a></div>
+    <div style='width: 640px; margin: 0 auto; text-align: left; padding: 0; overflow: hidden; color: black;'>
+      <div style='width: 75px; float: left;'><img src='http://www.ie6nomore.com/files/theme/ie6nomore-warning.jpg' alt='Warning!'/></div>
+      <div style='width: 275px; float: left; font-family: Arial, sans-serif;'>
+        <div style='font-size: 14px; font-weight: bold; margin-top: 12px;'>You are using an outdated browser</div>
+        <div style='font-size: 12px; margin-top: 6px; line-height: 12px;'>For a better experience using this site, please upgrade to a modern web browser.</div>
+      </div>
+      <div style='width: 75px; float: left;'><a href='http://www.firefox.com' target='_blank'><img src='http://www.ie6nomore.com/files/theme/ie6nomore-firefox.jpg' style='border: none;' alt='Get Firefox 3.5'/></a></div>
+      <div style='width: 75px; float: left;'><a href='http://www.browserforthebetter.com/download.html' target='_blank'><img src='http://www.ie6nomore.com/files/theme/ie6nomore-ie8.jpg' style='border: none;' alt='Get Internet Explorer 8'/></a></div>
+      <div style='width: 73px; float: left;'><a href='http://www.apple.com/safari/download/' target='_blank'><img src='http://www.ie6nomore.com/files/theme/ie6nomore-safari.jpg' style='border: none;' alt='Get Safari 4'/></a></div>
+      <div style='float: left;'><a href='http://www.google.com/chrome' target='_blank'><img src='http://www.ie6nomore.com/files/theme/ie6nomore-chrome.jpg' style='border: none;' alt='Get Google Chrome'/></a></div>
+    </div>
+  </div>
+  <![endif]-->
 <a name="Top" id="Top"></a>
 
 		
-        <div id="top_menu"><?php $this->load->view('global/menutop'); ?>	
+        <div id="top_menu">
+        	<?php $this->load->view('global/menutop'); ?>	
 		</div>
   
 
@@ -36,10 +50,12 @@
     	<div class="main clearfix"> 
             
                         <div id="ja-current-content" class="clearfix"> 
-               
+    
+     <div class="slideshow">          
       <?php	$this->load->view('global/slideshow1'); ?>
-                 
+     </div>            
 	<?php 
+	
 	if(isset($title))
 	{
 		echo "<h1 class='componentheading'>$title</h1>";
