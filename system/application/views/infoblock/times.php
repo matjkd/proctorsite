638ffd8999539b91b41c7c92f00c76b5b59390b0
mat sadler <github@redstudio.co.uk>
2010-08-25 10:05:34 +0100

    Deleted a load more useless files

--- system/application/views/infoblock/times.php
+++
@@ -1,5 +0,0 @@
-The Pre-schools opening hours are:<br/>
- 
-<p>Monday, Tuesday, Wednesday, Thursday and Friday mornings 9.05 a.m. - 11.35 a.m.<br/>
- 
-Monday, Tuesday, Wednesday, Thursday and Friday afternoons  12.15 p.m. - 2.45 p.m.</p>
--- system/application/views/presentations/main.php
+++
@@ -1,5 +0,0 @@
-Welcome to the Document Library section within Channel-Resource.com<br/>
-Here you will find an extensive library of Sales Tools and Product Resources available for you to use within the tabs below.
-<br/>
-
-<?php $this->load->view('presentations/tabs'); ?>
