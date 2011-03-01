c741671d90a41dbf6f8cfd9c34fe0753b3279973
mat sadler <mat@redstudio.co.uk>
2011-03-01 09:35:35 +0000

    email forms updated

--- system/application/views/forms/form_template.php
+++ system/application/views/forms/form_template.php
@@ -1,10 +1,19 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-gb" lang="en-gb">
 
-<?php $this->load->view('global/header'); ?>
+<?php $this->load->view('forms/header'); ?>
 
 <body> 
-	
+<div id="header">
+	<div id="logo">
+		<img src="<?=base_url()?>images/forms/leasedesk-logo.jpg"/>
+	</div>
+</div>	
 <?php $this->load->view($main); ?>
+
+<div id="footer">
+	
+	
+</div>
 </body>
 </html>
--- system/application/views/forms/webinar.php
+++ system/application/views/forms/webinar.php
@@ -1,11 +1,6 @@
-<script type="text/javascript">
-	$(function() {
-		$("#datepicker").datepicker({showOtherMonths: true, selectOtherMonths: true, dateFormat: 'D, dd M yy' });
-	});
-	</script>
-	<div align="center" style="width:400px; margin:20px auto; border:1px solid #888888; padding:5px;">
+<div  id="form">
 <h2>Register for Webinar</h2>
-on Thursday 24th Feb at 4pm
+on Thursday 24th Feb at 4pm<br/><br/>
 <?php echo $errors;?>	
 <?=form_open('forms/webinar')?>
 <?=$this->load->view('global/table')?>	
@@ -15,24 +10,50 @@ $data = array(
 			'name' => 'address',
 			'id' => 'address',
             'rows'   => '4',
-            'cols'        => '22'
+            'cols'        => '18'
             
             );
 
 
+echo form_fieldset('Firstname');
+echo form_input('firstname', set_value('firstname', $firstname));
+echo form_fieldset_close(); 
 
-$this->table->add_row('First Name', form_input('firstname', set_value('firstname', $firstname)));
-$this->table->add_row('Last Name', form_input('lastname', set_value('lastname', $lastname)));
-$this->table->add_row('Email', form_input('email', set_value('email', $email)));
-$this->table->add_row('Phone', form_input('phone', set_value('phone', $phone)));
-$this->table->add_row('Job Title', form_input('jobtitle', set_value('jobtitle', $jobtitle)));
-$this->table->add_row('Business Name', form_input('business_name', set_value('business_name', $business_name)));
-$this->table->add_row('Address', form_textarea($data, set_value('address', $address)));
+echo form_fieldset('Lastname');
+echo form_input('lastname', set_value('lastname', $lastname));
+echo form_fieldset_close(); 
+
+
+echo form_fieldset('Email');
+echo form_input('email', set_value('email', $email));
+echo form_fieldset_close(); 
+
+
+echo form_fieldset('Phone');
+echo form_input('phone', set_value('phone', $phone));
+echo form_fieldset_close(); 
+
+
+echo form_fieldset('Job Title');
+echo form_input('jobtitle', set_value('jobtitle', $jobtitle));
+echo form_fieldset_close(); 
+
+
+echo form_fieldset('Business Name');
+echo form_input('business_name', set_value('business_name', $business_name));
+echo form_fieldset_close(); 
+
+
+echo form_fieldset('Address');
+echo form_textarea($data, set_value('address', $address));
+echo form_fieldset_close(); 
+
+
 
 	
-	echo $this->table->generate();
-	$this->table->clear();
+echo "<br/>";	
 	echo form_submit('submit', 'Submit');
 		form_close();
 	
 ?>
+</div>
