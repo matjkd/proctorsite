c410b400d54a6df4b7d385cab55d3d23f046ef62
mat sadler <mat@redstudio.co.uk>
2011-04-05 17:09:53 +0100

    updated footer and css

---
+++ system/application/views/global/footer_menu.php
@@ -0,0 +1,15 @@
+
+
+<div id="bottom_menu">
+    		<ul>
+    		<li><a href="<?=base_url()?>">Home</a></li> 
+    	
+    		<li><a href="<?=base_url()?>leasedesk">Software Solutions</a></li>
+    		<li><a href="<?=base_url()?>welcome/content/consultancy">Consultancy</a></li>
+    		<li><a href="<?=base_url()?>welcome/content/peopledevelopment">People Development</a></li>
+    			
+    			<li><a href="<?=base_url()?>blog">Blog</a></li>
+    		<li><a href="<?=base_url()?>welcome/contact">Contact Us</a></li>
+    		<li><a target="_blank" href="http://www.customer-resource.com/welcome/directory">Terms and Conditions</a></li>
+    		</ul>
+</div>
--- system/application/views/sidebar/footerlinks.php
+++ system/application/views/sidebar/footerlinks.php
@@ -1,7 +1,12 @@
+
+<?=$this->load->view('global/footer_menu')?>
+
+<div>
 &copy; <?=$current_year?> <?php echo $config_company_name; ?>.
 
 
 <?=$config_address1?>,
 <?=$config_address5?>,
 <?=$config_address6?> <br/>
-<a target="_blank" href="http://www.customer-resource.com/welcome/directory">Terms and Conditions</a>
+</div>
+
