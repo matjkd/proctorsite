4b03c6f7b7674bbba8fe6d1ecdac9cd690244ad3
Mat Sadler <mat@redstudio.co.uk>
2011-06-21 15:24:36 +0100

    added twitter button

--- system/application/views/pages/news.php
+++ system/application/views/pages/news.php
@@ -16,24 +16,25 @@ if(isset($create_news))
 
 
 
-
 <?php endforeach;
 //list news here
 foreach($news as $news):?>
-<p><h3>
+
+<span class="blogcontainer">
+<p><h1>
 <?=$news['news_title'];?> <?php if(isset($edit))
 {
 	echo "  <a style='float:right;' href='".base_url()."admin/editnews/".$news['news_id']."'><img width='16px' height='16px' alt='edit' src='".base_url()."images/icons/social/edit_16.png'></a>";
 }
 ?>
-</h3>
+</h1>
 <?php
 
 	$old_date = strtotime($news['date_added']);
 	$new_date = date('l jS \of F Y', $old_date);
 
 ?>
-<div class="news_date"><?=$new_date?><br/>
+<div class="news_date"><?=$new_date?>, 
  Added by <?=$news['added_by'];?></div>
 
 </p>
@@ -58,9 +59,29 @@ echo $content;
 ?>
 
 
+
+<div  id="fb-root"></div><script src="http://connect.facebook.net/en_US/all.js#appId=195766550471199&amp;xfbml=1"></script>
+
 <a href="<?=base_url()?>blog/post/<?=$news['news_id']?>">Read More</a><br/>
 
-<g:plusone style="float:left;" width="55" height="20" href="<?=base_url()?>blog/post/<?=$news['news_id']?>"></g:plusone>
-<fb:like href="<?=base_url()?>blog/post/<?=$news['news_id']?>" show_faces="false" width="350" font=""></fb:like>
+<div class="social-single">
 
+
+<div id="twitterbutton">
+    <a href="http://twitter.com/share" class="twitter-share-button" data-count="horizontal" data-via="leasedeskdotcom">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+</div>
+
+
+
+    <div id="plusone">
+    <g:plusone size="medium" width="55px" height="20px" href="<?=base_url()?>blog/post/<?=$news['news_id']?>"></g:plusone>
+</div>
+
+    <div id="likebutton">
+    <fb:like href="<?=base_url()?>blog/post/<?=$news['news_id']?>" send="false" layout="button_count" width="0" height="20" show_faces="false" font=""></fb:like>
+</div>
+</div>
+<div style="clear:both"></div>
+</span>
 <?php endforeach; ?>
+
