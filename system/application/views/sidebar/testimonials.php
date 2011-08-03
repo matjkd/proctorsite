7043976eb28a60352f8a6df9529db83898fb2ba1
Mat Sadler <mat@redstudio.co.uk>
2011-08-03 16:32:23 +0100

    added hhf testimonial

--- system/application/views/sidebar/testimonials.php
+++ system/application/views/sidebar/testimonials.php
@@ -1,4 +1,6 @@
+<?php $random = rand(1,2); ?>
 
+<?php if($random == 1) { ?>
 <div style="background:url('/images/logos/companies/testimonials/daisy.png')  no-repeat;">
 		<div class="cycleside" style="display:none;">
 			<div>
@@ -21,5 +23,32 @@
 		
 		</div>
 </div>
+<?php } ?>
+
+<?php if($random == 2) { ?>
+<div style="background:url('/images/logos/companies/testimonials/hhf.png')  no-repeat;">
+		<div class="cycleside" style="display:none;">
+			<div>
+			<img src="<?=base_url()?>images/icons/speech_open_32.png">
+
+			<div id="testimonials">The biggest benefit
+is the ability to secure all data in one system, with access to any aspect of the business at the touch of a button</div>
+				<div style="float:right"><img src="<?=base_url()?>images/icons/speech_close_32.png"></div>
+			<br/>
+			<br/>
+
+
+				<div id="testimonial_name">
+				-- Jason Davies, Operations Director for Henry Howard Finance Plc.
+				</div>
+
+				<div style="clear:both"></div>
+			</div>
+
+
+
+		</div>
+</div>
+<?php } ?>
 	
 		
