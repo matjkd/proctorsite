49c94d35bba8607c91f76ed424c5f2ede46ab724
Mat Sadler <mat@redstudio.co.uk>
2011-08-17 09:11:23 +0100

    removed 1 testimonial

--- system/application/views/sidebar/testimonials.php
+++ system/application/views/sidebar/testimonials.php
@@ -26,20 +26,19 @@
 <?php } ?>
 
 <?php if($random == 2) { ?>
-<div style="background:url('/images/logos/companies/testimonials/hhf.png')  no-repeat;">
+<div style="background:url('/images/logos/companies/testimonials/daisy.png')  no-repeat;">
 		<div class="cycleside" style="display:none;">
 			<div>
 			<img src="<?=base_url()?>images/icons/speech_open_32.png">
 
-			<div id="testimonials"><a target="_blank" href="http://www.channel-resource.com/images/files/Henry_Howard_Finance_PLC_Case_Study.pdf">The biggest benefit
-is the ability to secure all data in one system, with access to any aspect of the business at the touch of a button</a></div>
+			<div id="testimonials"><a target="_blank" href="http://www.channel-resource.com/images/files/Daisy_PLC_Case_Study.pdf">The opportunities identified have been included in recent Daisy telemarketing campaigns that have successfully created additional business for the Group.</a></div>
 				<div style="float:right"><img src="<?=base_url()?>images/icons/speech_close_32.png"></div>
 			<br/>
 			<br/>
 
 
 				<div id="testimonial_name">
-				-- Jason Davies, Operations Director for Henry Howard Finance Plc.
+				-- Andrew Goldwater, Commercial Director Daisy Group Plc.
 				</div>
 
 				<div style="clear:both"></div>
