d1d78ff881122a77180d2b75159c14329148aeee
Mat Sadler <mat@redstudio.co.uk>
2012-03-14 15:30:37 +0000

    [#23008659] fixed tag cloud sizing

--- system/application/views/sidebar/tagcloud.php
+++ system/application/views/sidebar/tagcloud.php
@@ -5,17 +5,18 @@
     <?php 
     
     $lastone = '';
+    $lastsafe = '';
     $size = 1;
     $fontsize = 11;
     
     ?>
 <?php if($tagcloud !=NULL) { foreach($tagcloud as $row):?>
-    
+   
 <?php
 if($row->news_tag == $lastone) {
     $size = $size + 1;
 } else { ?>
-   <span style="font-size: <?php echo $fontsize+$size; ?>px;"><a href="<?=base_url()?>blog/tagged/<?=$row->news_tag_safe?>"><?=$row->news_tag?></a></span> 
+   <span style="font-size: <?php echo $fontsize+$size; ?>px;"><a href="<?=base_url()?>blog/tagged/<?=$lastsafe?>"><?=$lastone?></a></span> 
  <?php    $size = 1;
 }
 
@@ -25,7 +26,10 @@ if($row->news_tag == $lastone) {
 
 
 
-<?php $lastone = $row->news_tag;?>
 
+
+<?php $lastone = $row->news_tag;?>
+<?php $lastsafe = $row->news_tag_safe;?>
 <?php endforeach; } ?>
+ <span style="font-size: <?php echo $fontsize+$size; ?>px;"><a href="<?=base_url()?>blog/tagged/<?=$lastsafe?>"><?=$lastone?></a></span> 
 </div>
