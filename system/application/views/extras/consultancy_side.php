9873fed3c7490dc024993fa9d82c1c2be08175b2
Mat Sadler <mat@redstudio.co.uk>
2012-03-17 16:15:26 +0000

    changed popup rel

--- system/application/views/extras/consultancy_side.php
+++ system/application/views/extras/consultancy_side.php
@@ -4,31 +4,31 @@
  
 	<li> 
 	
-		<a href="#inline-1" rel="prettyPhoto"><img src="<?=base_url()?>images/icons/consultancy/outsourcing.png"/></a>
+		<a href="#inline-1" rel="consultancyPopup"><img src="<?=base_url()?>images/icons/consultancy/outsourcing.png"/></a>
 		
 	</li> 
 
 	<li> 
 		
-			<a href="#inline-2" rel="prettyPhoto"><img src="<?=base_url()?>images/icons/consultancy/financial.png"/></a>
+			<a href="#inline-2" rel="consultancyPopup"><img src="<?=base_url()?>images/icons/consultancy/financial.png"/></a>
 			
 	</li> 
 	
 	<li> 
 		
-			<a href="#inline-3" rel="prettyPhoto"><img src="<?=base_url()?>images/icons/consultancy/sales_marketing.png"/></a>
+			<a href="#inline-3" rel="consultancyPopup"><img src="<?=base_url()?>images/icons/consultancy/sales_marketing.png"/></a>
 			
 	</li> 
 	
 	<li> 
 		
-			<a href="#inline-4" rel="prettyPhoto"><img src="<?=base_url()?>images/icons/consultancy/commercial.png"/></a>
+			<a href="#inline-4" rel="consultancyPopup"><img src="<?=base_url()?>images/icons/consultancy/commercial.png"/></a>
 			
 	</li> 
 
 	<li> 
 		
-				<a href="#inline-5" rel="prettyPhoto"><img src="<?=base_url()?>images/icons/consultancy/channel_development.png"/></a>
+				<a href="#inline-5" rel="consultancyPopup"><img src="<?=base_url()?>images/icons/consultancy/channel_development.png"/></a>
 			
 	</li> 
 
