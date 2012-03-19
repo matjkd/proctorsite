fc3b7423e9be72b38d8161d8ece1bec46680f4ee
Mat Sadler <mat@redstudio.co.uk>
2012-03-19 10:13:14 +0000

    moved scroller script

--- js/script.js
+++ js/script.js
@@ -1,6 +1,45 @@
  var base_url = $('#baseurl').val();
  
  
+ //software scroller
+
+if($("#main_navi").length>0) {
+    // do 
+    //horizontal scroller
+    $(document).ready(function() {
+		 
+        //remove fixed height
+        $('#widecolumntop').css('height', 'auto'); 
+		 
+        // main vertical scroll
+        $("#mainscroller").scrollable({
+		 
+            // basic settings
+            vertical: true,
+		 
+            // up/down keys will always control this scrollable
+            keyboard: 'static',
+		 
+            // assign left/right keys to the actively viewed scrollable
+            onSeek: function(event, i) {
+                horizontal.eq(i).data("scrollable").focus();
+            }
+		 
+        // main navigator (thumbnail images)
+        }).navigator("#main_navi");
+		 
+        // horizontal scrollables. each one is circular and has its own navigator instance
+        var horizontal = $(".scrollable").scrollable({
+            circular: true
+        }).navigator(".navi");
+		 
+		 
+        // when page loads setup keyboard focus on the first horzontal scrollable
+       horizontal.eq(0).data("scrollable").focus();
+		 
+    });
+}
+ 
 
 // remap jQuery to $
 (function($){
@@ -401,44 +440,7 @@ $(function() {
 		
 });
 	
-//software scroller
 
-if($("#main_navi").length>0) {
-    // do 
-    //horizontal scroller
-    $(document).ready(function() {
-		 
-        //remove fixed height
-        $('#widecolumntop').css('height', 'auto'); 
-		 
-        // main vertical scroll
-        $("#mainscroller").scrollable({
-		 
-            // basic settings
-            vertical: true,
-		 
-            // up/down keys will always control this scrollable
-            keyboard: 'static',
-		 
-            // assign left/right keys to the actively viewed scrollable
-            onSeek: function(event, i) {
-                horizontal.eq(i).data("scrollable").focus();
-            }
-		 
-        // main navigator (thumbnail images)
-        }).navigator("#main_navi");
-		 
-        // horizontal scrollables. each one is circular and has its own navigator instance
-        var horizontal = $(".scrollable").scrollable({
-            circular: true
-        }).navigator(".navi");
-		 
-		 
-        // when page loads setup keyboard focus on the first horzontal scrollable
-       horizontal.eq(0).data("scrollable").focus();
-		 
-    });
-}
 //calculator
 
 $('.buttonsend').click(function (event) {
