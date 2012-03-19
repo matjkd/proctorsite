79b95431e057a673b06aacdc5ac62a7c731a349b
Mat Sadler <mat@redstudio.co.uk>
2012-03-19 10:20:00 +0000

    found missing character, replaced it

--- js/script.js
+++ js/script.js
@@ -1,6 +1,38 @@
  var base_url = $('#baseurl').val();
  
  
+
+ 
+
+// remap jQuery to $
+(function($){
+
+    })(this.jQuery);
+
+
+
+// usage: log('inside coolFunc',this,arguments);
+// paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
+window.log = function(){
+    log.history = log.history || [];   // store logs to an array for reference
+    log.history.push(arguments);
+    if(this.console){
+        console.log( Array.prototype.slice.call(arguments) );
+    }
+};
+
+
+
+// catch all document.write() calls
+(function(doc){
+    var write = doc.write;
+    doc.write = function(q){ 
+        log('document.write(): ',arguments); 
+        if (/docwriteregexwhitelist/.test(q)) write.apply(doc,arguments);  
+    };
+})(document);
+
+
  //software scroller
 $(function() {
 if($("#main_navi").length>0) {
@@ -40,36 +72,6 @@ if($("#main_navi").length>0) {
     });
 }
 });
- 
-
-// remap jQuery to $
-(function($){
-
-    })(this.jQuery);
-
-
-
-// usage: log('inside coolFunc',this,arguments);
-// paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
-window.log = function(){
-    log.history = log.history || [];   // store logs to an array for reference
-    log.history.push(arguments);
-    if(this.console){
-        console.log( Array.prototype.slice.call(arguments) );
-    }
-};
-
-
-
-// catch all document.write() calls
-(function(doc){
-    var write = doc.write;
-    doc.write = function(q){ 
-        log('document.write(): ',arguments); 
-        if (/docwriteregexwhitelist/.test(q)) write.apply(doc,arguments);  
-    };
-})(document);
-
 
 	
 // inline input titles
